bc5a75edaccf938a1799bf35eabd0de67c23d4c4
daeun.park <78911818+ekdms5566@users.noreply.github.com>
2023-02-17 22:01:09 +0900

    feat : 기본 규격 세팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    text-align: center;
+    margin: 0 auto;
+    max-width: 479px;
+  }
+`
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>test2</h1>
     </div>
   );
