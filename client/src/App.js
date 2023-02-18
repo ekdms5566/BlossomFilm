fb2b58f02ebc1f61745f1c56d3fd0d3b2ea8f6cd
Hyomk <qet557@gmail.com>
2023-02-18 19:40:56 +0900

    feat : 모바일 웹 사이즈 최적화

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,20 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
+    /*
+    모바일 환경에서 100vh 사용시 생기는 스크롤 버그 문제 방지 코드
+    사용하고자 하는 페이지 최상당 layout 사이즈  height: calc(var(--vh, 1vh) * 100) 로 설정하여 사용
+    */
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+    setScreenSize();
+    window.addEventListener("resize", setScreenSize);
+
     return (
         <div className="App">
             <GlobalStyle />
-            <h1>test2</h1>
         </div>
     );
 }
