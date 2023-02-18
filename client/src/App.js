6c53da4029f8f71e468a68596aababf78aef1497
Hyomk <qet557@gmail.com>
2023-02-18 19:28:06 +0900

    fix :  상단 여백 제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import { createGlobalStyle } from 'styled-components'
-
+import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
+ *{
+      margin: 0;
+      padding: 0;
+  }
   body {
     text-align: center;
     margin: 0 auto;
     max-width: 479px;
   }
-`
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <h1>test2</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <h1>test2</h1>
+        </div>
+    );
 }
 
 export default App;
