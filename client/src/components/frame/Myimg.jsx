a125b5aca57586f149db028fc2f8428041c4738f
seokhyun Yoon <gah1234@naver.com>
2023-02-20 22:20:38 +0900

    image preview solution
    사진 업로드 & 미리보기 해결

--- client/src/components/frame/Myimg.jsx
+++ client/src/components/frame/Myimg.jsx
@@ -25,29 +25,32 @@ const Input = styled.input`
 
 
 export default function Myimg(props) {
+    const [files, setFiles] = useState({File1 : "", File2 : "", File3: "", File4: ""});
     const onloadFile = (e) => {
-        e.preventDefault();
-        (URL.createObjectURL(e.target.files[0]));
-        props.upload(props.id, true, files);
+        const image = e.target.id;
+        const imgURL = URL.createObjectURL(e.target.files[0]);
+        setFiles({...files, [image]:imgURL})
     }
-
-    console.log(props.id);
+    console.log(files);
 
     return (
         <section>
             <form method="post" enctype="multipart/form-data">
-                
-                    {!files && <Label for="chooseFile1">+231123312312123</Label>}
-                    {!files && <Label for="chooseFile2">+231123312312123</Label>}
-                    {!files && <Label for="chooseFile3">+</Label>}
-                    {!files && <Label for="chooseFile4">+</Label>}
-                
-                <Input type="file" id="chooseFile1" name="chooseFile" accept="image/*" onChange={onloadFile} />
-                <Input type="file" id="chooseFile2" name="chooseFile" accept="image/*" onChange={onloadFile} />
-                <Input type="file" id="chooseFile3" name="chooseFile" accept="image/*" onChange={onloadFile} />
-                <Input type="file" id="chooseFile4" name="chooseFile" accept="image/*" onChange={onloadFile} />
+                <div>
+                    {!files.File1 && <Label for="File1">+</Label>}
+                    {!files.File2 && <Label for="File2">+</Label>}
+                    {!files.File3 && <Label for="File3">+</Label>}
+                    {!files.File4 && <Label for="File4">+</Label>}
+                </div>
+                <Input type="file" id="File1" name="chooseFile" accept="image/*" onChange={onloadFile} />
+                <Input type="file" id="File2" name="chooseFile" accept="image/*" onChange={onloadFile} />
+                <Input type="file" id="File3" name="chooseFile" accept="image/*" onChange={onloadFile} />
+                <Input type="file" id="File4" name="chooseFile" accept="image/*" onChange={onloadFile} />
             </form>
-            {files && <div><Img alt="sample" src={files} /></div>}
+            {files.File1 && <div><Img alt="sample" src={files.File1} /></div>}
+            {files.File2 && <div><Img alt="sample" src={files.File2} /></div>}
+            {files.File3 && <div><Img alt="sample" src={files.File3} /></div>}
+            {files.File4 && <div><Img alt="sample" src={files.File4} /></div>}
         </section>
     );
 }
