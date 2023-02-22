de111cf22f5079043199e46f028f4f071a2e8a78
ekdms5566 <ekdms5566@naver.com>
2023-02-22 14:42:23 +0900

    fix : 프레임 선택 페이지 반응형 수정

--- client/src/components/DefaultFrame/style.js
+++ client/src/components/DefaultFrame/style.js
@@ -1,24 +1,24 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  height: 260px;
-  width: 340px;
+  width: 100vw;
   display: flex;
   justify-content: space-around;
   align-items: center;
 `;
 
 export const ArrowBtn = styled.button`
-  width: 30px;
-  height: 30px;
+  width: 3rem;
+  height: 3rem;
   z-index: 10;
   background-color: transparent;
   border: none;
 `;
 
 export const ContImg = styled.img`
-  /* width: 250px; */
-  height: 170px;
+  max-width: 70%;
+  max-height: 19rem;
+  margin: 30px 0;
   box-shadow: 2px 3px 3px 0.2px rgba(0, 0, 0, 0.1);
   background-color: #f4f4f4;
 `;
