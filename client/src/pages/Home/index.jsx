3d7bdd736dcd514b4e1c0697f1de5646bb1f84c4
ekdms5566 <ekdms5566@naver.com>
2023-02-25 19:04:44 +0900

    Feat : snowfall 추가

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,10 +3,24 @@ import Logo from "../../assets/logo.svg"
 import SubLogo from "../../assets/sub-logo.svg"
 import Button from "../../components/Button"
 import * as S from "./style"
+import Petal from "../../assets/petal.png"
+import Snowfall from "react-snowfall"
+
+const snowflake1 = document.createElement("img");
+snowflake1.src = Petal;
+
+const images = [snowflake1, snowflake1];
 
 export default function Home() {
   return (
     <S.Wrapper>
+    <Snowfall
+            snowflakeCount={50}
+            radius={[12, 15]}
+            color="pink"
+            speed={[0.05, 0.08]}
+            images={images}
+        />
       <S.Subtit>우리의 <S.SubSpan>"봄"</S.SubSpan>을 담다</S.Subtit>
       <h1>
         <img src={Logo} alt="" />
